d6acf70e0b064a208cfb49b17ed5cdeeb47fd4fe
hexpe <multidevelop.java@gmail.com>
2022-03-14 01:40:47 +0300

    ending

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ new TypeIt("#main_title", {
   waitUntilVisible: true,
   cursor: false,
 }).go();
+
+// Show the first tab and hide the rest
+$("#tabs-nav .tab:first-child .link").addClass("active");
+$(".tab-content").hide();
+$(".tab-content:first").show();
+
+// Click function
+$("#tabs-nav .link").click(function () {
+  $("#tabs-nav .link").removeClass("active");
+  $(this).addClass("active");
+  $(".tab-content").hide();
+
+  const activeTab = $(this).attr("href");
+  $(activeTab).fadeIn();
+  return false;
+});
